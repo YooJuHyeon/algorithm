dffe27b2559ea81195ca7ed30c756dc12b2a5a27
YooJuHyeon <121422559+YooJuHyeon@users.noreply.github.com>
2023-02-01 13:54:59 +0900

    [Bronze III] Title: 오븐 시계, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.py
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.py
@@ -1,9 +1,10 @@
-A, B = list(map(int, input().split()))
+A, B = map(int, input().split())
 C = int(input())
 
 if B + C > 59:
     A += (B+C)//60
     B = (B+C)%60
+
     if A > 23:
         A = A-24
 else:
