dba03211f5c83d22f953b13da049a88bb4285c3b
YooJuHyeon <121422559+YooJuHyeon@users.noreply.github.com>
2023-02-20 17:12:21 +0900

    [level 3] Title: 즐겨찾기가 가장 많은 식당 정보 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
+++ 프로그래머스/lv3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
@@ -1,10 +1,11 @@
 -- REST_INFO 테이블에서 음식종류별로 즐겨찾기수가 가장 많은 식당의 음식 종류, ID, 식당 이름, 즐겨찾기수를 조회
 -- 결과는 음식 종류를 기준으로 내림차순 정렬
+-- 왜 HAVING 말고, WHERE절에 서브쿼리를 써써 해야 하는지를 잘 모르겠음
+-- 그룹바이에 조건을 주는게 아니니까...
+
 SELECT FOOD_TYPE, REST_ID, REST_NAME, FAVORITES
 FROM REST_INFO
 WHERE FAVORITES IN (SELECT MAX(FAVORITES) FROM REST_INFO GROUP BY FOOD_TYPE)
 GROUP BY FOOD_TYPE
 # HAVING MAX(FAVORITES)
 ORDER BY FOOD_TYPE DESC;
-
-
