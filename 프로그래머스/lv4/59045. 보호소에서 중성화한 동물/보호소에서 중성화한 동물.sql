0970fd3a7993c76fd3d9a68ffd6e73b9a83a0f0e
YooJuHyeon <121422559+YooJuHyeon@users.noreply.github.com>
2023-02-20 14:32:58 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/lv4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -1,6 +1,7 @@
 -- ANIMAL_OUTS 테이블의 ANIMAL_ID는 ANIMAL_INS의 ANIMAL_ID의 외래 키입니다.
 --  보호소에 들어올 당시에는 중성화되지 않았지만, 보호소를 나갈 당시에는 중성화된 동물의 아이디와 생물 종, 이름을 조회하는 아이디 순으로 조회
 -- 중성화를 거치지 않은 동물은 성별 및 중성화 여부에 Intact, 중성화를 거친 동물은 Spayed 또는 Neutered라고 표시되어있습니다.
+-- WHERE 조건문 괄호로 묶기!!
 
 SELECT A1.ANIMAL_ID, A1.ANIMAL_TYPE, A1.NAME
 FROM ANIMAL_INS AS A1
